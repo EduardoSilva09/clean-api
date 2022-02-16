f2eab54a391c841ae568818648ccb25963188987
Eduardo Silva Feitosa <88010447+EduardoSilva09@users.noreply.github.com>
2022-02-15 21:33:08 -0300

    move generics errors to utils layer and refactor

---
+++ src/Utils/errors/index.js
@@ -0,0 +1,4 @@
+const MissingParamError = require('./missing-param-error.js')
+const InvalidParamError = require('./invalid-param-error.js')
+
+module.exports = { MissingParamError, InvalidParamError }
--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,6 +1,4 @@
-const MissingParamError = require('./missing-param-error.js')
-const InvalidParamError = require('./invalid-param-error.js')
 const ServerError = require('./server-error.js')
 const UnauthorizedError = require('./unauthorized-error.js')
 
-module.exports = { MissingParamError, InvalidParamError, ServerError, UnauthorizedError }
+module.exports = { ServerError, UnauthorizedError }
